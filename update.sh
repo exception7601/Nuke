f508653e283728ef1b0abe711f4720e99c9d5893
and <77030548+exception7601@users.noreply.github.com>
2025-02-27 14:09:24 -0300

    feat: add Carthage pre-build framework

--- update.sh
+++ update.sh
@@ -1,7 +1,10 @@
 NAME=nuke-xcframeworks-all-platforms.zip
-NEW_NAME=nuke-xcframeworks.zip
 REPO=kean/Nuke 
-
+BUILD=$(date +%s) 
+BUILD_COMMIT=$(git log --oneline --abbrev=16 --pretty=format:"%h" -1)
+JSON_FILE="Carthage/NukeBinary.json"
+NEW_NAME=nuke-iOS-${BUILD_COMMIT}.zip
+MY_REPO=exception7601/Nuke
 VERSION=$(gh release list \
   --repo ${REPO} \
   --exclude-pre-releases \
@@ -22,29 +25,45 @@ unzip -o ${NAME}
 zip -rX ${NEW_NAME} Nuke.xcframework
 
 SUM=$(swift package compute-checksum ${NEW_NAME} )
+DOWNLOAD_URL="https://github.com/${MY_REPO}/releases/download/${VERSION}/${NEW_NAME}"
 
-echo ${VERSION} > version
-git add version
+if [ ! -f $JSON_FILE ]; then
+  echo "{}" > $JSON_FILE
+fi
+
+# Make Carthage
+JSON_CARTHAGE="$(jq --arg version "${VERSION}" --arg url "${DOWNLOAD_URL}" '. + { ($version): $url }' $JSON_FILE)" 
+echo $JSON_CARTHAGE > $JSON_FILE
+
+git add $JSON_FILE
 git commit -m "new Version ${VERSION}"
-# git tag -s -a ${VERSION} -m "v${VERSION}"
-git checkout -b release-v${VERSION}
+# git push origin HEAD
+# echo ${VERSION} > version
+# git add version
+# git commit -m "new Version ${VERSION}"
+# git checkout -b release-v${VERSION}
+git tag -s -a ${VERSION} -m "v${VERSION}"
 git push origin HEAD --tags
 
 gh release create ${VERSION} ${NEW_NAME} --notes "checksum \`${SUM}\`"
 
-URL=$(gh release view ${VERSION} \
-  --repo exception7601/Nuke \
-  --json assets \
-  -q '.assets[0].apiUrl'
-)
-
 NOTES=$(cat <<END
+Carthage
+\`\`\`
+binary "https://raw.githubusercontent.com/${MY_REPO}/main/${JSON_FILE}"
+\`\`\`
+
+Install
+\`\`\`
+carthage bootstrap --use-xcframeworks
+\`\`\`
+
 SPM binaryTarget
 
 \`\`\`
 .binaryTarget(
   name: "Nuke",
-  url: "${URL}.zip",
+  url: "${DOWNLOAD_URL}",
   checksum: "${SUM}"
 )
 \`\`\`
