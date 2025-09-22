2bc9a0ae44ef22d0ff0097784d72a3af8c2ca8ee
exception7601 <77030548+exception7601@users.noreply.github.com>
2025-09-22 14:58:57 -0300

    new Version 12.8.0

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     .library(
       name: "Nuke",
       targets: [
-        "Sentry",
+        "Nuke",
       ]
     ),
   ],
@@ -17,8 +17,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Nuke",
-      url: "https://github.com/exception7601/Nuke/releases/download/12.8.0/nuke-iOS-89d4a5921c91dd21.zip",
-      checksum: "e9e7a69a8b2bc0585d5624996c8b15cd8e0be601bc60d10d1b0cd148bc0697bc"
+      url: "https://github.com/exception7601/Nuke/releases/download/12.8.0/nuke-iOS-8cff9120de35fde1.zip",
+      checksum: "c205d1ab0cc291b4172fcbb1ef807ec314f31911b805dd8b728da2d3400834bd"
     )
   ]
 )
