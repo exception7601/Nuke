f453f2423e539fcc93bf658d7148b98ced0c79a0
exception7601 <77030548+exception7601@users.noreply.github.com>
2025-09-24 12:16:39 -0300

    new Version 12.8.0

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     .binaryTarget(
       name: "Nuke",
       url: "https://github.com/exception7601/Nuke/releases/download/12.8.0/nuke-0ead44350d2737db.zip",
-      checksum: ""
+      checksum: "bfde2d4f75dd82a7e6ad7f3c4553147c165adef402d672880d826b820fe790c0"
     )
   ]
 )
